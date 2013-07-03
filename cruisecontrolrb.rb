6567fad5093cf0cd32e93e93acaacdd0d91bf1f1
Mikhail Nikalyukin <idups1k@gmail.com>
2013-07-03 11:13:12 +0300

    add support for multiple projects, change status link to display name of the project

--- cruisecontrolrb.rb
+++ cruisecontrolrb.rb
@@ -2,33 +2,36 @@ require 'httparty'
 require 'nokogiri'
 
 class Cruisecontrolrb
-  
+
   include HTTParty
-  
-  def initialize base_url, username = nil, password = nil
+
+  def initialize base_url, username = nil, password = nil, number
     @auth = { :username => username, :password => password }
     @base_url = base_url
+    @number = number - 1
   end
-  
+
   def fetch
     options = { :basic_auth => @auth }
 
     noko = Nokogiri::XML(self.class.get("http://#{@base_url}/XmlStatusReport.aspx", options).parsed_response)
 
-    return {} unless noko.search("Project").first
-    
-    status_hash = { :lastBuildStatus => noko.search("Project").first.attributes["lastBuildStatus"].value,
-      :webUrl => noko.search("Project").first.attributes["webUrl"].value,
-      :lastBuildLabel => noko.search("Project").first.attributes["lastBuildLabel"].value,
-      :activity => noko.search("Project").first.attributes["activity"].value }
+    return {} unless project = noko.search("Project")[@number]
+    status_hash = { :lastBuildStatus => project.attributes["lastBuildStatus"].value,
+      :webUrl => project.attributes["webUrl"].value,
+      :lastBuildLabel => project.attributes["lastBuildLabel"].value,
+      :activity => project.attributes["activity"].value,
+      :projectName => project.attributes["name"].value }
 
-    link_text = status_hash[:activity] == "Building" ? "build" : status_hash[:lastBuildStatus]
+    link_text = status_hash[:projectName] + ": "
+    link_text += status_hash[:activity] == "Building" ? "build" : status_hash[:lastBuildStatus]
+
     url = status_hash[:webUrl].gsub("projects", "builds")
-    
-    status_hash[:link_to_build] = "<a href=\"" + url + "/" + status_hash[:lastBuildLabel] + 
+
+    status_hash[:link_to_build] = "<a href=\"" + url + "/" + status_hash[:lastBuildLabel] +
       "\">" + link_text + "</a>"
-      
+
     status_hash
   end
-  
+
 end
--- cruisecontrolrb_to_hipchat.rb
+++ cruisecontrolrb_to_hipchat.rb
@@ -3,35 +3,36 @@ require "./cruisecontrolrb"
 require "./hipchat"
 
 class CruisecontrolrbToHipchat < Sinatra::Base
-    
+
   attr_accessor :status
   attr_accessor :activity
-  
-  scheduler = Rufus::Scheduler.start_new
-  
-  scheduler.every("#{ENV["POLLING_INTERVAL"] || 1}m") do  
-    
-    status_hash = Cruisecontrolrb.new(ENV["CC_URL"], ENV["CC_USERNAME"] || "", ENV["CC_PASSWORD"] || "").fetch
 
-    unless status_hash.empty?        
-      if status_hash[:activity] == "Building" and @activity != "Building"
-        Hipchat.new.hip_post "CruiseControl has started a #{status_hash[:link_to_build]}."
-        @activity = "Building"
-      # there might be a more clever way to structure this.
-      elsif (@activity.nil? and @status.nil?) or 
-            (@activity == "Building" and status_hash[:activity] != "Building")
-        @activity = status_hash[:activity]
-        @status = status_hash[:lastBuildStatus]
-      
-        color = status_hash[:lastBuildStatus] == "Success" ? "green" : "red"
-      
-        message = "Current+build+status:+#{status_hash[:link_to_build]}"
-          
-        Hipchat.new.hip_post message, color
+  scheduler = Rufus::Scheduler.start_new
+
+  scheduler.every("#{ENV["POLLING_INTERVAL"] || 1}m") do
+    ENV["PROJECT_NUMBERS"].to_i.times do |project_number|
+      status_hash = Cruisecontrolrb.new(ENV["CC_URL"], ENV["CC_USERNAME"] || "", ENV["CC_PASSWORD"] || "", project_number).fetch
+
+      unless status_hash.empty?
+        if status_hash[:activity] == "Building" and @activity != "Building"
+          Hipchat.new.hip_post "CruiseControl has started a #{status_hash[:link_to_build]}."
+          @activity = "Building"
+        # there might be a more clever way to structure this.
+        elsif (@activity.nil? and @status.nil?) or
+              (@activity == "Building" and status_hash[:activity] != "Building")
+          @activity = status_hash[:activity]
+          @status = status_hash[:lastBuildStatus]
+
+          color = status_hash[:lastBuildStatus] == "Success" ? "green" : "red"
+
+          message = "Current+build+status:+#{status_hash[:link_to_build]}"
+
+          Hipchat.new.hip_post message, color
+        end
       end
     end
   end
-  
+
   get "/" do
     "howdy!"
   end
